0cd5bb01aaa96ffa7cff3b2c3aa03ac6d801ffee
Vikash Pathak <vikashh.pathak@gmail.com>
2019-11-28 08:05:27 +0530

    added functionality to fetch / display pokemons data

--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloClient } from 'apollo-client';
+import {InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from 'react-apollo';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const cache = new InMemoryCache();
+const link = new HttpLink({
+    uri: 'https://graphql-pokemon.now.sh/'
+})
+
+const client = new ApolloClient({
+    cache,
+    link
+})
+
+
+ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'));
+
 serviceWorker.unregister();
